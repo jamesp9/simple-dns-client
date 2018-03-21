d7a78596e5c1a2b28a7b6ec7707a9f5ba93e1a35
jamesp9 <james@jp-it.net.au>
2018-03-22 00:41:51 +1100

    add tests

---
+++ functional_tests.py
@@ -0,0 +1,34 @@
+import unittest
+
+from dns_client import QueryDNS
+
+
+GOOGLE_NAME_SERVER = '8.8.8.8'
+JP_IT_IP_ADDRESS = '52.62.133.15'
+
+
+class CheckSimpleDnsClient(unittest.TestCase):
+    """
+    """
+
+    def setUp(self):
+        """
+        Use Google's DNS server to lookup my own domain which only has one ip
+        address.
+        """
+        query_dns = QueryDNS('jp-it.net.au', GOOGLE_NAME_SERVER)
+        self.decoded_stdout = query_dns.main().split('\n')
+
+    def test_number_of_answers(self):
+        """
+        Check an answer of 1 is returned
+        """
+        response_num_answers = self.decoded_stdout[6]
+        self.assertEqual(response_num_answers, 'Number of Answers: 1')
+
+    def test_domain_answer(self):
+        """
+        Check the answer returned by the DNS server.
+        """
+        response_answer = self.decoded_stdout[7]
+        self.assertEqual(response_answer, JP_IT_IP_ADDRESS)
---
+++ tests.py
@@ -0,0 +1,33 @@
+import unittest
+
+from dns_client import QueryDNS
+
+
+GOOGLE_NAME_SERVER = '8.8.8.8'
+JP_IT_IP_ADDRESS = '52.62.133.15'
+
+
+class CheckSimpleDnsClient(unittest.TestCase):
+    """
+    """
+
+    def setUp(self):
+        """
+        Use Google's DNS server to lookup my own domain which only has one ip
+        address.
+        """
+        self.query_dns = QueryDNS('jp-it.net.au', GOOGLE_NAME_SERVER)
+
+    def test_pack_header_fields(self):
+        """
+        """
+        packed_header = self.query_dns.pack_header_fields(42, 0x100, 0, 0, 0, 0)
+        self.assertEqual(packed_header,
+                         b'\x00*\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+
+    def test_question_construction(self):
+        """
+        """
+        packed_question = self.query_dns.construct_question()
+        self.assertEqual(packed_question,
+                         b'\x05jp-it\x03net\x02au\x00\x00\x01\x00\x01')
