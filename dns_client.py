25155bc94147bbbfb27c80136e8778b9929895e3
jamesp9 <james@jp-it.net.au>
2018-03-22 23:51:13 +1100

    finish first round of refactoring

--- dns_client.py
+++ dns_client.py
@@ -1,12 +1,7 @@
-'''
+"""
+Based on information from:
 RFC 1035 - Domain names - Implementation and Specification
-*  2.3.4 Size limits  *
-labels          63 octets or less, the two high order bits are 0
-names           255 octets or less
-TTL             positive values of a signed 32 bit number.
-UDP messages    512 octets or less
-'''
-
+"""
 import random
 import socket
 import struct
@@ -115,7 +110,11 @@ class QueryDNS:
         return struct.pack('>{0}sHH'.format(len(qname)), qname, qclass, qtype)
 
     def receive_response(self):
-        """"""
+        """
+        Wait for the DNS query response, will exit if response takes longer
+        that 2 seconds.
+        Returns a byte string of UDP data.
+        """
         delay = 0.1
         while True:
             self.udp_sock.settimeout(delay)
@@ -125,7 +124,7 @@ class QueryDNS:
                 delay *= 2  # exponential back off
                 if delay > 2.0:
                     self.print_output("Delay is more than 2.0 seconds.")
-                    self.udp_sock.close()
+                    self.close_socket()
                     sys.exit(3)
             except Exception as e:
                 self.print_output("A further exception occurred: {}".format(e))
@@ -134,7 +133,8 @@ class QueryDNS:
 
         return data
 
-    def unpack_header_fields(self, response_data):
+    @staticmethod
+    def unpack_header_fields(response_data):
         """
                                         1  1  1  1  1  1
           0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
@@ -153,14 +153,158 @@ class QueryDNS:
         +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
         """
         # The header is 6 short integers
-        return struct.unpack('>HHHHHH', response_data[:HEADER_SIZE_BYTES])
+        field_values = struct.unpack('>HHHHHH', response_data[:HEADER_SIZE_BYTES])
+        return {
+            'identifier': field_values[0],
+            'flags': field_values[1],
+            'qd_count': field_values[2],
+            'an_count': field_values[3],
+            'ns_count': field_values[4],
+            'ar_count': field_values[5],
+        }
 
-    def response_header_id(self):
+    @staticmethod
+    def decipher_header_flags(values):
+        """
+        Get all the fields values out of the Header
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        """
+        response_codes = {
+            '0': 'No Error',
+            '1': 'Format error',
+            '2': 'Server failure',
+            '3': 'Name Error',
+            '4': 'Not Implemented',
+            '5': 'Refused',
+        }
+        rcode = values & 0x000F
+        if str(rcode) in response_codes.keys():
+            response_code = (rcode, response_codes[str(rcode)])
+        elif 6 <= rcode <= 15:
+            response_code = (rcode, 'Reserved for future use')
+
+        flags = {
+            'recursion_available': 1 if values & 0x0080 == 128 else 0,
+            'response_code': response_code,
+
+        }
+
+        return flags
+
+    @staticmethod
+    def get_question_section(response_data):
+        """
+        Question Section
+                                        1  1  1  1  1  1
+          0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        |                                               |
+        /                     QNAME                     /
+        /                                               /
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        |                     QTYPE                     |
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        |                     QCLASS                    |
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        """
+        offset = HEADER_SIZE_BYTES
+        section = {}
+        # QNAME
+        labels = []
+        while True:
+            length_octet = response_data[offset]
+            offset += 1
+            # The domain name terminates with the zero length octet
+            if length_octet == 0:
+                break
+
+            label = response_data[offset:offset + length_octet]
+            labels.append(label.decode())
+            offset += length_octet
+
+        section['qname'] = '.'.join(labels)
+
+        # QTYPE
+        section['qtype'] = response_data[offset: offset + 2]
+        offset += 2
+
+        # QCLASS
+        section['qclass'] = response_data[offset: offset + 2]
+        offset += 2
+
+        return section, offset
+
+    def get_answer_section(self, response_data, offset, answer_count):
+        """
+        Answer Section
+
+        Resource record format
+                                        1  1  1  1  1  1
+          0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        |                                               |
+        /                                               /
+        /                      NAME                     /
+        |                                               |
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        |                      TYPE                     |
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        |                     CLASS                     |
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        |                      TTL                      |
+        |                                               |
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        |                   RDLENGTH                    |
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--|
+        /                     RDATA                     /
+        /                                               /
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        """
+        section = {}
+        # Response - Resource Record - Answers
+        address_count = 0
+        for an in range(1, answer_count + 1):
+            # NAME
+            rrname = response_data[offset: offset + 2]
+            rrname = struct.unpack('>H', rrname)[0]
+            if rrname & 0xc000:
+                pass
+                # print("rr name is compressed: offset {0}".format(rrname & 0x3fff))
+            offset += 2
+
+            # TYPE, CLASS, TTL, RDLENDTH
+            type_class_ttl_rdlength = response_data[offset: offset + 10]
+            section['type'], section['class'], section['ttl'], section['rdlength'] = struct.unpack(
+                '>HHLH', type_class_ttl_rdlength)
+            # print("RR - TYPE:{0} CLASS:{1} TTL:{2} RDLENGTH:{3}".format(rrtype,rrclass,rrttl,rrrdlength))
+            offset += 10
+
+            # RDATA
+            if (section['type'] == 1 and section['class'] == 1 and
+                    section['rdlength'] == 4):
+                section['rdata'] = response_data[offset: offset + 4]
+                self.print_output(socket.inet_ntoa(section['rdata']))
+                offset += 4
+                address_count += 1
+            else:
+                offset += section['rdlength']
+
+        section['address_count'] = address_count
+
+        return section
+
+    def check_response_header_id(self, response_identifier):
         """"""
-        if response_header[0] != self.identifier:
-            print("Transaction ID's do not match", response_header[0], self.identifier)
+        if response_identifier != self.identifier:
+            print('Transaction IDs do not match {} {}'.format(
+                response_identifier, self.identifier))
             sys.exit(4)
 
+    def close_socket(self):
+        """"""
+        self.udp_sock.close()
 
     def main(self):
         """
@@ -191,91 +335,37 @@ class QueryDNS:
         response_data = self.receive_response()
         self.print_output("Bytes received: {0}".format(len(response_data)))
 
-        # Response - Header
-        response_header = self.unpack_header_fields(response_data)
-        # Response - Header - ID
-        response_identifier = self.response_header_id(response_header)
-        self.print_output("Identifier: {0}".format(self.identifier))
+        response_header_fields = self.unpack_header_fields(response_data)
+        response_header_flags = self.decipher_header_flags(
+            response_header_fields['flags'])
 
-        # Response - Header - Flags - RA - Recursion Available
-        ra = response_header[1] & 0x0080
-        if ra == 128:
-            ra = 1
-        self.print_output("Recursion supported: {0}".format(ra))
+        self.print_output("Recursion supported: {0}".format(
+            response_header_flags['recursion_available']))
 
-        # Response - Header - Flags - RCODE - Response Code
-        rcode = response_header[1] & 0x000F
-        if rcode == 0:
-            rcode_str = "No Error"
-        elif rcode == 1:
-            rcode_str = "Format error"
-        elif rcode == 2:
-            rcode_str = "Server failure"
-        elif rcode == 3:
-            rcode_str = "Name Error"
-        elif rcode == 4:
-            rcode_str = "Not Implented"
-        elif rcode == 5:
-            rcode_str = "Refused"
-        else:
-            rcode_str = "Reserved for furture use"
-        self.print_output("Query Status: {0} ({1})".format(rcode, rcode_str))
+        self.print_output("Identifier: {0}".format(response_header_fields['identifier']))
 
-        answer_count = response_header[3]
-        self.print_output("Number of Answers: {0}".format(answer_count))
+        # RCODE - Response Code
+        self.print_output("Query Status: {0} ({1})".format(
+            *response_header_flags['response_code']))
 
-        # Response - Question - QNAME
-        while True:
-            lo = response_data[offset]  # Length octet
-            offset += 1
-            if lo == 0:
-                break
+        # Answer Count
+        answer_count = response_header_fields['an_count']
+        self.print_output('Number of Answers: {0}'.format(answer_count))
 
-            label = response_data[offset:offset + lo]
-            # print(lo, label)
-            offset += lo
-
-        # Response - Question - QTYPE
-        qtype = response_data[offset: offset + 2]
-        offset += 2
+        # Question Section
+        question_section, offset = self.get_question_section(response_data)
         # print("QTYPE: {0}".format(struct.unpack('>H' , qtype)[0]))
-
-        # Response - Question - QCLASS
-        qclass = data[offset: offset + 2]
-        offset += 2
         # print("QCLASS: {0}".format(struct.unpack('>H' , qclass)[0]))
 
-        # Response - Resource Record - Answers
-        add_cnt = 0
-        for an in range(1, answer_count + 1):
-            # Response - Resource Record - NAME
-            rrname = data[offset: offset + 2]
-            rrname = struct.unpack('>H', rrname)[0]
-            if rrname & 0xc000:
-                pass
-                # print("rr name is compressed: offset {0}".format(rrname & 0x3fff))
-            offset += 2
+        # Answer Section
+        answer_section = self.get_answer_section(response_data, offset, answer_count)
 
-            # Response - Resource Record - TYPE, CLASS, TTL, RDLENDTH
-            rrfixed = data[offset: offset + 10]
-            rrtype, rrclass, rrttl, rrrdlength = struct.unpack(">HHLH", rrfixed)
-            # print("RR - TYPE:{0} CLASS:{1} TTL:{2} RDLENGTH:{3}".format(rrtype,rrclass,rrttl,rrrdlength))
-            offset += 10
-
-            # Response - Resource Record - RDATA
-            if rrtype == 1 and rrclass == 1 and rrrdlength == 4:
-                rdata = data[offset: offset + 4]
-                self.print_output(socket.inet_ntoa(rdata))
-                offset += 4
-                add_cnt += 1
-            else:
-                offset += rrrdlength
-
-        self.print_output("Number of addresses found: {0}".format(add_cnt))
+        self.print_output("Number of addresses found: {0}".format(
+            answer_section['address_count']))
 
         self.print_output("End of processing.")
 
-        self.udp_sock.close()
+        self.close_socket()
 
         return self.OUTPUT
 
